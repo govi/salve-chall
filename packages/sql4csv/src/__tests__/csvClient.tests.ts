059ea12cbe2a3c3dbed85807511c144d99398d91
Govi Ramanujam <govi@email.com>
2024-03-04 01:25:53 +0000

    fixed tests and cleaned stuff up

--- packages/sql4csv/src/__tests__/csvClient.tests.ts
+++ packages/sql4csv/src/__tests__/csvClient.tests.ts
@@ -8,8 +8,9 @@ const defaultOptions: CSVClientOptions = {
 describe("csv client tests", () => {
   it("should read csv content and return an object", async () => {
     const client = new CSVClient(defaultOptions)
-    const result = await client.query<{ id: string; name: string }>("clinics")
-    expect(result).toBeDefined()
+    const resp = await client.query<{ id: string; name: string }>("clinics")
+    expect(resp).toBeDefined()
+    const result = resp.items
     expect(result.length).toBe(2)
     expect(result[0].id).toBe("1")
     expect(result[0].name).toBe("Salve Fertility")
@@ -18,10 +19,11 @@ describe("csv client tests", () => {
 
   it("should return only subset of fields requested", async () => {
     const client = new CSVClient(defaultOptions)
-    const result = await client.query<{ id: string; name: string }>("clinics", [
+    const resp = await client.query<{ id: string; name: string }>("clinics", [
       "name",
     ])
-    expect(result).toBeDefined()
+    expect(resp).toBeDefined()
+    const result = resp.items
     expect(result.length).toBe(2)
     expect(result[0].id).toBeUndefined()
     expect(result[0].name).toBe("Salve Fertility")
@@ -30,11 +32,11 @@ describe("csv client tests", () => {
 
   it("should ignore fields that do not exist", async () => {
     const client = new CSVClient(defaultOptions)
-    const result = await client.query<{ id: string; name: string }>("clinics", [
+    const resp = await client.query<{ id: string; name: string }>("clinics", [
       "bingo",
     ])
-    expect(result).toBeDefined()
-    expect(result.length).toBe(2)
+    expect(resp).toBeDefined()
+    const result = resp.items
     expect(result[0].id).toBeUndefined()
     expect(result[0].name).toBeUndefined()
     expect(result[1].name).toBeUndefined()
@@ -49,13 +51,13 @@ describe("csv client tests", () => {
 
   it("should throw when requesting tenant owned items without an tenant-id", async () => {
     const client = new CSVClient(defaultOptions)
-    const result = await client.query<{ id: string; date_of_birth: string }>(
+    const resp = await client.query<{ id: string; date_of_birth: string }>(
       "patients",
       ["date_of_birth"],
       { clinic_id: 2 }
     )
-    expect(result).toBeDefined()
-    expect(result.length).toBe(3)
-    expect(result[2].date_of_birth).toBe("1983-11-23")
+    expect(resp).toBeDefined()
+    const result = resp.items
+    expect(result[2].date_of_birth).toBe("1983-11-23T00:00:00.000Z")
   })
 })
